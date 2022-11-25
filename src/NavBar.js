72048808f13e29d0d0c5aa2d3386ef39b3d35b70
Golffffff <matergmgolf@gmail.com>
2022-11-25 20:13:43 +0700

    first commit

--- src/NavBar.js
+++ src/NavBar.js
@@ -15,7 +15,7 @@ export const NavBar = () =>{
             <Container>
                 <Navbar.Brand>
                     <img src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="" height="20"></img>
-                <babel style={Brandstyle} > Kornmongkhon Ruesingpisch Project</babel>
+                <babel style={Brandstyle} > Kittipat Chaipranitan Project</babel>
                 </Navbar.Brand>
                     <Nav className='home'>
                         <Nav.Link href="/Home" style={Homebar}>Home</Nav.Link>
